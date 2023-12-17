ef4b7ac6b0b5b9a4ccc7a1e9075ee8f5c96071e0
Sh1wOo <zuromr@gmail.com>
2023-12-17 05:11:15 +0300

    feat: added rechart example

--- clients/user-ui/src/app/page.tsx
+++ clients/user-ui/src/app/page.tsx
@@ -1,10 +1,20 @@
-import { Button } from '@/shared/ui/button'
-import Image from 'next/image'
+import { Overview } from "@/shared/components/rechart.example";
+import { Button } from "@/shared/components/ui/button";
+import { Card, CardContent, CardHeader} from "@/shared/components/ui/card";
 
 export default function Home() {
   return (
     <main>
       <Button>Hello, world!</Button>
+      <Card className="w-[540px] mx-auto">
+        <CardHeader>
+          <h2>Overview</h2>
+        </CardHeader>
+
+        <CardContent className="pl-2">
+          <Overview />
+        </CardContent>
+      </Card>
     </main>
-  )
+  );
 }
