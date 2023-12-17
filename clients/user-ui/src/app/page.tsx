714b903aab187b6db380319dbb564d8ba3975941
Sh1wOo <zuromr@gmail.com>
2023-12-17 04:54:09 +0300

    init: nextjs v14 app with tailwind shadcn/ui and rechart

---
+++ clients/user-ui/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/shared/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <Button>Hello, world!</Button>
+    </main>
+  )
+}
